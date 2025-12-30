7a0fa49d0fa5e24da5776aa845ad8329637fd560
Emil Hoffmann <81522556+emil-ho@users.noreply.github.com>
2025-12-30 13:40:30 +0100

    Remove copyright and license comments from optimizer.py
    Removed copyright and licensing comments from the top of the file.

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,9 +1,7 @@
 # taken from: https://github.com/facebookresearch/schedule_free/tree/main/schedulefree
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
-# 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+
 from typing import Tuple, Union, Optional, Iterable, Dict, Callable, Any
 from typing_extensions import TypeAlias
 import torch
